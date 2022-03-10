b8f52f71d5e5906ade20c6d1307a9bbd533b1fb5
Prayag Lehana <prayag.lehana02@gmail.com>
2022-03-11 00:59:09 +0530

    google authentication

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import Chat from "./components/Chat/Chat";
 import Pusher from "pusher-js";
 import { useEffect, useState } from "react";
 import axios from "./axios";
+import { gapi } from "gapi-script";
+import LoginButton from "./login";
+
+const clientId =
+  "728833144528-jnqafjlf2ogq6p7r7qn7lmkf22iulvs9.apps.googleusercontent.com";
 
 function App() {
   const [messages, setMessages] = useState([]);
-
   useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    }
+
+    gapi.load("client:auth2", start);
+
     axios.get("/api/v1/messages/get").then((res) => {
       console.log("res Dtaa ", res.data);
       setMessages(res.data);
@@ -32,6 +45,8 @@ function App() {
   }, [messages]);
   return (
     <div className="app">
+      <LoginButton />
+
       {console.log("messae :::: ", messages)}
       <div className="app__body">
         <Sidebar />
---
+++ src/login.js
@@ -0,0 +1,27 @@
+import { GoogleLogin } from "react-google-login";
+
+const clientId =
+  "728833144528-jnqafjlf2ogq6p7r7qn7lmkf22iulvs9.apps.googleusercontent.com";
+
+const onSuccess = (res) => {
+  console.log("LOGIN Success", res.profileObj);
+};
+const onFailure = (res) => {
+  console.log("LOGIN Success", res.profileObj);
+};
+function Login() {
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default Login;
